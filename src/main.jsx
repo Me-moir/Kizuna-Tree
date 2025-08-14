52d13423858acfe234dce1fed76714ee388b3633
Jekk <johncenjekktesnado@gmail.com>
2025-08-14 15:30:50 +0800

    Simplify: use root domain instead of /TNK-linktree path

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,10 @@
-// Install react-router-dom first:
-// npm install react-router-dom
-
-// src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        {/* Redirect root to TNK-linktree */}
-        <Route path="/" element={<Navigate to="/TNK-linktree" replace />} />
-        
-        {/* Your linktree page */}
-        <Route path="/TNK-linktree" element={<App />} />
-        
-        {/* 404 fallback */}
-        <Route path="*" element={<Navigate to="/TNK-linktree" replace />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
 )
