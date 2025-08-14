a18bb24c14ddfa0efec4730e9d29fd95ee8a8d66
Jekk <johncenjekktesnado@gmail.com>
2025-08-14 15:26:52 +0800

    Add routing for /TNK-linktree path

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
+// Install react-router-dom first:
+// npm install react-router-dom
+
+// src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        {/* Redirect root to TNK-linktree */}
+        <Route path="/" element={<Navigate to="/TNK-linktree" replace />} />
+        
+        {/* Your linktree page */}
+        <Route path="/TNK-linktree" element={<App />} />
+        
+        {/* 404 fallback */}
+        <Route path="*" element={<Navigate to="/TNK-linktree" replace />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
